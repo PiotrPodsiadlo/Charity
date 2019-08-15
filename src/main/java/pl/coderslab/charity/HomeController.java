7a05a0453c0741324f5a8b67bf3c4066db960676
Piotr <piotrekchorzow@interia.eu>
2019-08-16 00:20:40 +0200

    preparing list of all institutions

--- src/main/java/pl/coderslab/charity/HomeController.java
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -4,14 +4,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import pl.coderslab.charity.institution.Institution;
+import pl.coderslab.charity.institution.InstitutionService;
 
 
 @Controller
 public class HomeController {
 
+    InstitutionService institutionService;
+
+    public HomeController(InstitutionService institutionService) {
+        this.institutionService = institutionService;
+    }
 
     @RequestMapping("/")
     public String homeAction(Model model){
+        model.addAttribute("institutions", institutionService.findAllInstitutions());
         return "index";
     }
 }
--- src/main/java/pl/coderslab/charity/institution/InstitutionService.java
+++ src/main/java/pl/coderslab/charity/institution/InstitutionService.java
@@ -1,4 +1,24 @@
 package pl.coderslab.charity.institution;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
 public class InstitutionService {
+
+    InstitutionRepository institutionRepository;
+
+    @Autowired
+    public InstitutionService(InstitutionRepository institutionRepository) {
+        this.institutionRepository = institutionRepository;
+    }
+
+
+    public List<Institution> findAllInstitutions() {
+        return institutionRepository.findAll();
+    }
 }
