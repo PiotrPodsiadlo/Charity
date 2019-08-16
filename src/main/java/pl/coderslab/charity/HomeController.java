9eec94804df3785f7b352a6259fd20a14e459d07
Piotr <piotrekchorzow@interia.eu>
2019-08-16 02:44:27 +0200

    fixed number of given bags

--- src/main/java/pl/coderslab/charity/HomeController.java
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -1,9 +1,12 @@
 package pl.coderslab.charity;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import pl.coderslab.charity.category.CategoryService;
+import pl.coderslab.charity.donation.DonationService;
 import pl.coderslab.charity.institution.Institution;
 import pl.coderslab.charity.institution.InstitutionService;
 
@@ -12,14 +15,20 @@ import pl.coderslab.charity.institution.InstitutionService;
 public class HomeController {
 
     InstitutionService institutionService;
+    DonationService donationService;
+    CategoryService categoryService;
 
-    public HomeController(InstitutionService institutionService) {
+    @Autowired
+    public HomeController(InstitutionService institutionService, DonationService donationService, CategoryService categoryService) {
         this.institutionService = institutionService;
+        this.donationService = donationService;
+        this.categoryService = categoryService;
     }
 
     @RequestMapping("/")
     public String homeAction(Model model){
         model.addAttribute("institutions", institutionService.findAllInstitutions());
+        model.addAttribute("trashbags", donationService.countAllBags());
         return "index";
     }
 }
--- src/main/java/pl/coderslab/charity/donation/DonationService.java
+++ src/main/java/pl/coderslab/charity/donation/DonationService.java
@@ -1,4 +1,30 @@
 package pl.coderslab.charity.donation;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
 public class DonationService {
+
+    DonationRepository donationRepository;
+
+    @Autowired
+    public DonationService(DonationRepository donationRepository) {
+        this.donationRepository = donationRepository;
+    }
+
+    public Long countAllBags(){
+        Long numberOfDonations = donationRepository.count();
+        Long overallNumOfBags = 0L;
+        for (long i = 0; i < numberOfDonations; i++) {
+            long numberOfBagsInThisDonation  = donationRepository.getOne(i).getQuantity();
+            overallNumOfBags += numberOfBagsInThisDonation;
+        }
+        return overallNumOfBags;
+
+    }
 }
